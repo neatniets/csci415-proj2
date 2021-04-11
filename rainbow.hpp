e26f6f7a37c71f9089d7ce90c5c3949d1b0330c4
niets <mymailboxrebelled@gmail.com>
2021-04-11 17:13:17 -0500

    Init header

---
+++ rainbow.hpp
@@ -0,0 +1,44 @@
+#ifndef RAINBOW_HPP
+#define RAINBOW_HPP
+
+/** Set of characters allowed in user passwords. */
+static const char charset[]
+	= "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+typedef unsigned char uchar_t;
+
+/** Precompute a hash chain, obtaining the endpoint password.
+ * @post	returned password should be freed
+ * @return	endpoint password */
+char *
+hash_chain(
+	uchar_t len, //!<[in] hash chain length
+	const char *salt, //!<[in] salt string
+	const char *passwd //!<[in] startpoint password string
+);
+
+/** Create a hash from a salt and a password.
+ * @post        hash must be freed
+ * @return      hash string */
+char *
+hash(
+	const char *salt, //!<[in] salt string
+	const char *passwd //!<[in] password string
+);
+
+/** Free memory allocated to a hash. */
+void
+free_hash(char *hash); //!<[in] hash string
+
+/** Reduce a hash to a password string.
+ * A different reduction function is used depending on the key.
+ * Only 256 reduction functions are supported.
+ * @post        password string must be freed
+ * @return      password string */
+char *
+reduce(
+	uchar_t key, //!<[in] key for reduction function
+	const char *hash //!<[in] hash string
+);
+
+#endif /* !RAINBOW_HPP */
