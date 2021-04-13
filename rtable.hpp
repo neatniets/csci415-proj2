e1a8a10bea89be382b7c3bd425e47999beaa5183
niets <mymailboxrebelled@gmail.com>
2021-04-13 00:18:30 -0500

    Clarify wording in documenation

--- rtable.hpp
+++ rtable.hpp
@@ -4,8 +4,11 @@
 #include <stddef.h>
 
 /** Build a rainbow table.
+ * The filename for the table may be NULL; a filename will be generated.
+ *
  * The password dictionary contains passwords on a single line separated by a
  * single newline.
+ *
  * Each line of the rainbow table will be written in the following format:
  * "%zu %s %zu\n", salt, endpoint, offset
  * The endpoint is the last password in that hash chain,
@@ -15,8 +18,8 @@
  * @return	0 on success */
 int
 build_rbtable(
-	const char *dpath, //!<[in] path to password dictionary
-	const char *tpath, //!<[in] optional path to rainbow table
+	const char *dfile, //!<[in] filename of password dictionary
+	const char *tfile, //!<[in] optional filename of rainbow table
 	size_t salt_max //!<[in] maximum salt value to create table for
 );
 
